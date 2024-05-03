2aa81fa05554fdb457f9b7bf69ca85e0cc4f2154
Krutarth Bhavani <kbhavani@stevens.edu>
2024-05-02 23:38:06 -0400

    Add HeroSection component to Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <div className="relative w-full flex item-center justify-center">
-        <Navbar/>
+        <div className="relative w-full flex items-center justify-center ">
+          <Navbar />
         </div>
         {children}
       </body>
